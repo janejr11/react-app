fb3c00202ed49f14b539949b29889c0604e96507
Jack Janes <11janesj@gmail.com>
2020-04-19 18:53:12 -0400

    update scripts, add split-pane

--- react/src/App.js
+++ react/src/App.js
@@ -1,62 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SplitPane, { Pane } from 'react-split-pane';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SplitPane split="vertical" minSize={50}>
-          <div>
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and foo to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </div>
-          <SplitPane split="horizontal">
-            <div>
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and foo to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </div>
-            <div>
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and foo to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </div>
-          </SplitPane>
-        </SplitPane>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and foo to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
 
-
+    // <SplitPane split="vertical" minSize={50}>
+    //   <div />
+    //   <SplitPane split="horizontal">
+    //     <div />
+    //     <div />
+    //   </SplitPane>
+    // </SplitPane>
   );
 }
 
